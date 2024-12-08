737e199626911a130880ac802e93d5569de734c4
unknown <bibektotol@gmail.com>
2024-12-09 00:15:06 +0600

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import Cards from '../Cards/Cards';
+import MoreQuestion from '../MoreQuestion/MoreQuestion';
+import MoreFeatures from '../MoreFeature/MoreFeatures';
+const Home = () => {
+    return (
+        <div  className='bg-[#080325] z-1  max-w-md lg:max-w-6xl mx-auto'>
+            <Banner></Banner>
+            <MoreFeatures></MoreFeatures>
+            <Cards></Cards>
+            <MoreQuestion></MoreQuestion>
+        </div>
+    );
+};
+
+export default Home;
